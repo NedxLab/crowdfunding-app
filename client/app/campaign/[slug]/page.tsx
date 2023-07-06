c15e422731daec4a384937602a036e5a602095fe
NedxLab <odoc047@gmail.com>
2023-07-06 01:49:16 +0100

    text size

--- client/app/campaign/[slug]/page.tsx
+++ client/app/campaign/[slug]/page.tsx
@@ -165,7 +165,7 @@ export default function Page({ params }: { params: { slug: string } }) {
       {campaigns ? (
         <>
           {" "}
-          <h1 className={`"text-4xl font-bold text-center py-8`}>
+          <h1 className="text-4xl font-bold text-center py-8">
             Campaign Details
           </h1>
           <div
